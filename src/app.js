90eec5a2c2560dcba89b9a49a68fe08ca02fb43a
kirak98 <kirak98@hotmail.co.uk>
2023-05-24 11:54:26 +0200

    Forecast JS and css added

--- src/app.js
+++ src/app.js
@@ -18,6 +18,37 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
   }
+
+  function displayForecast() {
+    let forecastElement = document.querySelector("#forecast");
+
+   
+    let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
+
+    let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src=""
+          alt="" id=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 18° </span>
+          <span class="weather-forecast-temperature-min"> 12° </span>
+        </div>
+      </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+}
   
   function displayTemperature(response) {
       let temperatureElement = document.querySelector("#temperature");
@@ -84,3 +115,4 @@ function formatDate(timestamp) {
   celsiusLink.addEventListener("click", displayCelsiusTemperature);
   
   search("Barcelona");
+  displayForecast();
